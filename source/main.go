06a50b31258311e56944a2621f3bb6728e302177
Raul David Nota Mercado <rnota@gruposion.bo>
2018-11-26 12:30:20 -0400

    add post request

--- source/main.go
+++ source/main.go
@@ -4,10 +4,12 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
 	"io"
 	"io/ioutil"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
 )
@@ -20,8 +22,11 @@ type Document struct {
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/documents", getDocuments).Methods("GET")
-	router.HandleFunc("/documents/{id}", getDocument).Methods("GET")
+	router.HandleFunc("/documents", getDocuments).Methods(http.MethodGet)
+	router.HandleFunc("/documents/{id}", getDocument).Methods(http.MethodGet)
+	router.HandleFunc("/documents", createdDocument).Queries("name", "{name}").Methods(http.MethodPost)
+	router.HandleFunc("/documents/{id}", delDocument).Methods(http.MethodDelete)
+
 	log.Fatal(http.ListenAndServe(":9000", router))
 }
 
@@ -30,6 +35,7 @@ func getDocuments(w http.ResponseWriter, r *http.Request) {
 	docs = listFiles()
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(docs)
 }
 
@@ -39,10 +45,64 @@ func getDocument(w http.ResponseWriter, r *http.Request) {
 	doc = getFile(params["id"])
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(doc)
 }
 
-const pathFolder = "C:\\Users\\rdavi\\Downloads\\Programs"
+func delDocument(w http.ResponseWriter, r *http.Request) {
+	var doc Document
+	params := mux.Vars(r)
+	doc = getFile(params["id"])
+
+	deleteFile(doc.Name)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(doc)
+}
+
+func createdDocument(w http.ResponseWriter, r *http.Request) {
+
+	var filename = r.URL.Query().Get("name")
+	fmt.Println(filename)
+	file, _, err := r.FormFile("document")
+
+	check(err)
+
+	defer file.Close()
+
+	var doc = createdFile(filename, file)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(doc)
+}
+
+func createdFile(filename string, file multipart.File) Document{
+	f, err := os.OpenFile(pathFolder+"\\"+filename, os.O_WRONLY|os.O_CREATE, 0666)
+
+	check(err)
+	defer f.Close()
+
+	io.Copy(f, file)
+
+	return getFile(calculateChecksum(filename))
+}
+
+func deleteFile(filename string) {
+	if len(filename) > 0 {
+		error := os.Remove(pathFolder + "\\" + filename)
+		check(error)
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+const pathFolder = "C:\\Users\\UTI01\\Downloads\\Programs"
 
 func getFile(id string) Document {
 	var result Document

5be9d6841bcd017eba146a514f246a98aa5be12a
core persephone <jr.panozo@gmail.com>
2018-12-10 21:35:41 -0400

    fix bug

--- source/commands/mail/config.go
+++ source/commands/mail/config.go
@@ -1,8 +1,8 @@
 package mail
 
 var mailConfig = map[string]string{
-	"username": "rdavid.nota@gmail.com",
-	"password": "Mercado1nota",
+	"username": "user@gmail.com",
+	"password": "pass",
 	"subject":  "Final project",
 	"host":     "smtp.gmail.com",
 	"port":     "587",
